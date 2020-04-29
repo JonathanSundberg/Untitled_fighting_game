041a1f41a2dcf4b0bf15fc5a8733dbbce64d5b64
Henrik Vik <henkvik@gmail.com>
2020-04-29 21:00:18 +0200

    added discord and text mesh pro, testing discord lobbies

---
+++ Assets/Scripts/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class GameObjectExtensions
+    {
+        public static IEnumerable<GameObject> GetChildren(this GameObject game_object)
+        {
+            return game_object.transform.GetChildren().Select(child => child.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/TransformExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class TransformExtensions
+    {
+        public static IEnumerable<Transform> GetChildren(this Transform transform)
+        {
+            var childCount = transform.childCount;
+            for (var childIndex = 0; childIndex < childCount; childIndex++)
+            {
+                yield return transform.GetChild(childIndex);
+            }
+        }
+    }
+}
