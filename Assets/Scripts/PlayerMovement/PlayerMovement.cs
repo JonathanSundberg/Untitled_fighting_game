d148ed7278ea46f391de43a93a04824df3f0ab6e
Jonathan Sundberg <guitaraddict95@hotmail.com>
2020-05-01 18:59:11 +0200

    Super basic movement

---
+++ Assets/Scripts/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector2 movement_dir;
+    float MoveSpeed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+        Vector2 movement = new Vector2(movement_dir.x, movement_dir.y) * MoveSpeed * Time.deltaTime;
+        transform.Translate(movement);
+
+    }
+}
---
+++ Assets/Scripts/Playerinput/playercontroller.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class playercontroller : MonoBehaviour
+{
+
+    PlayerMovement movement;
+    private PlayerInput _input;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movement = gameObject.GetComponent<PlayerMovement>();
+        _input = GetComponent<PlayerInput>();
+        _input.actions["Up"].performed += OnUp;
+        _input.actions["Up"].canceled += VerticalCancel;
+        _input.actions["Down"].performed += OnDown;
+        _input.actions["Down"].canceled += VerticalCancel;
+        _input.actions["Left"].performed += OnLeft;
+        _input.actions["Left"].canceled += HorizontalCancel;
+        _input.actions["Right"].performed += OnRight;
+        _input.actions["Right"].canceled += HorizontalCancel;
+
+    }
+
+    private void OnUp(InputAction.CallbackContext value)
+    {
+        Debug.Log("Up!");
+        var input = value.ReadValue<float>();
+        Debug.Log(input);
+        movement.movement_dir.y = input;
+    }
+    private void VerticalCancel(InputAction.CallbackContext value)
+    {
+        Debug.Log("Vertical canceled");
+        movement.movement_dir.y = 0;
+    }
+    private void HorizontalCancel(InputAction.CallbackContext value)
+    {
+        Debug.Log("Horizontal canceled");
+        movement.movement_dir.x = 0;
+    }
+
+    private void OnDown(InputAction.CallbackContext value)
+    {
+        Debug.Log("Down!");
+        var input = value.ReadValue<float>();
+        Debug.Log(input);
+        movement.movement_dir.y = input *-1.0f;
+    }
+
+    private void OnLeft(InputAction.CallbackContext value)
+    {
+        Debug.Log("Left!");
+        var input = value.ReadValue<float>();
+        Debug.Log(input);
+        movement.movement_dir.x = input * -1.0f;
+    }
+
+    private void OnRight(InputAction.CallbackContext value)
+    {
+        Debug.Log("Right!");
+        //var input = value.ReadValue<float>();
+        //Debug.Log(input);
+        //movement.movement_dir.x = input ;
+    }
+
+
+}
