61172c1e01fe002e53d3cc1c9dbb2e1c5caa1868
Jonathan Sundberg <guitaraddict95@hotmail.com>
2020-05-23 20:50:24 +0200

    Added camerafollow

---
+++ Assets/Scripts/camera/CameraFollow.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour
+{
+    public List<Transform> targets;
+    public Vector3 offset;
+    private Vector3 velocity;
+    public float smoothTime = .001f;
+
+    public float minZoom = 50;
+    public float maxZoom = 10f;
+    public float zoomLimiter = 200;
+
+    private Camera cam;
+    private void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    private void LateUpdate()
+    {
+        if (targets.Count == 0)
+            return;
+
+        Move();
+        Zoom();
+        
+    }
+    void Zoom()
+    {
+        float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance()/zoomLimiter);
+        cam.fieldOfView = newZoom;
+
+
+    }
+
+    void Move()
+    {
+        Vector3 centerPoint = GetCenterPoint();
+        Vector3 newPosition = centerPoint + offset;
+        //transform.position = Vector3.SmoothDamp(transform.position, newPosition, ref velocity, smoothTime);
+        transform.position = newPosition;
+    }
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i<targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.size.x;
+    }
+    private Vector3 GetCenterPoint()
+    {
+        if (targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+            
+    }
+}
